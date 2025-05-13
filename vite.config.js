3d71040b05204738e68042a96d77de321fbe6f6e
A1dan <90688234+a1-leap@users.noreply.github.com>
2025-05-13 13:12:10 +0100

    Update vite.config.js
    create base / for custom domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/leap-portal-25/', // Make sure this matches your repository name exactly
+  base: '/', // Make sure this matches your repository name exactly
   build: {
     assetsDir: 'assets', // Ensure assets go in a predictable directory
     // Ensure file names don't use hash in development for easier debugging
